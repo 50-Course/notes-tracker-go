8a2bb50064e30eb85c18818cb05c84e844c84746
50-Course <erioluwaadeodu@my.uopeople.edu>
2025-03-18 11:29:29 +0100

    feat: implement and setup handlers for grpc communication

--- cmd/grpc/server.go
+++ cmd/grpc/server.go
@@ -1,1 +1,144 @@
 package grpc
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"time"
+
+	"github.com/50-Course/notes-tracker/cmd/repository"
+	"github.com/50-Course/notes-tracker/shared/models"
+	api "github.com/50-Course/notes-tracker/shared/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type TaskServiceServer struct {
+	api.UnimplementedTaskServiceServer
+	repo *repository.TaskRepository
+}
+
+// Creates new instance of TaskServiceServer
+func NewTaskServiceServer(repo *repository.TaskRepository) *TaskServiceServer {
+	return &TaskServiceServer{repo: repo}
+}
+
+// Handles our CreateTask RPC call for creating tasks
+func (s *TaskServiceServer) CreateTask(ctx context.Context, req *api.CreateTaskRequest) (*api.CreateTaskResponse, error) {
+	if req.Title == "" {
+		return nil, fmt.Errorf("Title is required")
+	}
+
+	task := &models.Task{
+		Title:       req.Title,
+		Description: req.Description,
+		CreatedAt:   time.Now(),
+	}
+
+	err := s.repo.CreateTask(ctx, task)
+	if err != nil {
+		// just propagate that error up our handler
+		return nil, fmt.Errorf("Error creating task: %v", err)
+	}
+
+	return &api.CreateTaskResponse{
+		Task: &api.Task{
+			Id:          task.ID,
+			Title:       task.Title,
+			Description: task.Description,
+			CreatedAt:   task.CreatedAt.String(),
+		},
+	}, nil
+}
+
+// Handles call to get a specific task
+func (s *TaskServiceServer) GetTask(ctx context.Context, req *api.GetTaskRequest) (*api.GetTaskResponse, error) {
+	task, err := s.repo.GetTask(ctx, req.Id)
+	if err != nil {
+		return nil, fmt.Errorf("Task not found: %w", err)
+	}
+
+	return &api.GetTaskResponse{
+		Task: &api.Task{
+			Id:          task.ID,
+			Title:       task.Title,
+			Description: task.Description,
+			CreatedAt:   task.CreatedAt.String(),
+		},
+	}, nil
+}
+
+// Fetches all tasks
+func (s *TaskServiceServer) ListTasks(ctx context.Context, req *api.ListTasksRequest) (*api.ListTasksResponse, error) {
+	tasks, err := s.repo.ListTasks(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("Error fetching tasks: %v", err)
+	}
+
+	var grpcTasks []*api.Task
+	for _, task := range tasks {
+		grpcTasks = append(grpcTasks, &api.Task{
+			Id:          task.ID,
+			Title:       task.Title,
+			Description: task.Description,
+			CreatedAt:   task.CreatedAt.String(),
+		})
+	}
+
+	return &api.ListTasksResponse{Tasks: grpcTasks}, nil
+}
+
+// Handles our UpdateTask RPC call for updating tasks
+func (s *TaskServiceServer) UpdateTask(ctx context.Context, req *api.UpdateTaskRequest) (*api.UpdateTaskResponse, error) {
+	task, err := s.repo.GetTask(ctx, req.Id)
+	if err != nil {
+		return nil, fmt.Errorf("Task not found: %w", err)
+	}
+
+	task.Title = req.Title
+	task.Description = req.Description
+
+	err = s.repo.UpdateTask(ctx, task)
+	if err != nil {
+		return nil, fmt.Errorf("Error updating task: %v", err)
+	}
+
+	return &api.UpdateTaskResponse{
+		Task: &api.Task{
+			Id:          task.ID,
+			Title:       task.Title,
+			Description: task.Description,
+			CreatedAt:   task.CreatedAt.String(),
+		},
+	}, nil
+}
+
+// Handles our RPC call for deleting tasks
+func (s *TaskServiceServer) DeleteTask(ctx context.Context, req *api.DeleteTaskRequest) (*api.DeleteTaskResponse, error) {
+	err := s.repo.DeleteTask(ctx, req.Id)
+	if err != nil {
+		return nil, fmt.Errorf("Error deleting task: %v", err)
+	}
+
+	return &api.DeleteTaskResponse{Success: true}, nil
+}
+
+// StartServer starts the gRPC server
+func RunGRPCServer(repo *repository.TaskRepository, port string) {
+	listen, err := net.Listen("tcp", port)
+	if err != nil {
+		fmt.Errorf("Failed to start GRPC server: %v", err)
+		return
+	}
+
+	server := grpc.NewServer()
+	api.RegisterTaskServiceServer(server, &TaskServiceServer{repo: repo})
+
+	reflection.Register(server)
+	fmt.Printf("Starting gRPC server on %s\n", port)
+	fmt.Printf("gRPC server started on port %s\n", port)
+
+	if err := server.Serve(listen); err != nil {
+		fmt.Printf("Failed to start GRPC server: %v", err)
+	}
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,61 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	_ "fmt"
+	"log"
+	_ "net"
+	"os"
+
+	grpcserver "github.com/50-Course/notes-tracker/cmd/grpc"
+	"github.com/50-Course/notes-tracker/cmd/repository"
+	_ "github.com/50-Course/notes-tracker/shared/proto"
+	"github.com/joho/godotenv"
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/pgdialect"
+)
+
+func connectToDB(dbUrl string) (*bun.DB, error) {
+	db, err := sql.Open("pg", dbUrl)
+	if err != nil {
+		return nil, fmt.Errorf("Error connecting to database: %v", err)
+	}
+	return bun.NewDB(db, pgdialect.New()), nil
+}
+
 func main() {
-	// This is the main function
+	if err := godotenv.Load("config/.env"); err != nil {
+		log.Fatal("Error loading .env file. Please confirm file exists in the right file path and try again.")
+	}
+
+	dbUrl, exists := os.LookupEnv("DATABASE_URL")
+	if !exists {
+		log.Fatal("DATABASE_URL not set in environment")
+	}
+
+	internalServerPort, exists := os.LookupEnv("INTERNAL_SERVER_PORT")
+	if !exists {
+		log.Printf("INTERNAL_SERVER_PORT not set in environment. Defaulting to 50051")
+		// we should just default to some open unused port
+		internalServerPort = "50051"
+	}
+
+	db, err := connectToDB(dbUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// we would then initialize our grpc server here
+	// server := grpc.NewServer()
+	repo := repository.NewTaskRepository(db)
+	grpcserver.RunGRPCServer(repo, internalServerPort)
+
+	// api.RegisterTaskServiceServer(server, &grpcserver.TaskServerServer{repo: repo})
+
+	log.Printf("gRPC Server started on port %s", internalServerPort)
+
+	if err := server.Serve(listner); err != nil {
+		log.Fatalf("Failed to start server on %s", err)
+	}
 }
