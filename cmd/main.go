a6f69a24cfe3dc3e841d2937443ec9449d63efa8
50-Course <erioluwaadeodu@my.uopeople.edu>
2025-03-18 21:14:00 +0100

    refactor: implement builder function for dynamic db url generation

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"database/sql"
-	 "fmt"
+	"fmt"
 	"log"
 	_ "net"
 	"os"
@@ -17,12 +17,33 @@ import (
 	_ "github.com/uptrace/bun/driver/pgdriver"
 )
 
+func buildDatabaseURL() string {
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	host := os.Getenv("POSTGRES_HOST")
+	port := os.Getenv("POSTGRES_PORT")
+	dbname := os.Getenv("POSTGRES_DB")
+
+	if user == "" || password == "" || host == "" || port == "" || dbname == "" {
+		log.Fatal("Missing required database environment variables")
+	}
+
+	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", user, password, host, port, dbname)
+}
+
 func connectToDB(dbUrl string) (*bun.DB, error) {
 	log.Println("Connecting to database with URL:", dbUrl)
 	db, err := sql.Open("postgres", dbUrl)
 	if err != nil {
 		return nil, fmt.Errorf("Error connecting to database: %v", err)
 	}
+
+	// ping db to truely know if we are connecting to the databse
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("Failed to connect to database: %v", err)
+	}
+
+	log.Println("Connected to database successfully")
 	return bun.NewDB(db, pgdialect.New()), nil
 }
 
@@ -31,10 +52,10 @@ func main() {
 		log.Fatal("Error loading .env file. Please confirm file exists in the right file path and try again.")
 	}
 
-	dbUrl, exists := os.LookupEnv("DATABASE_URL")
-	if !exists {
-		log.Fatal("DATABASE_URL not set in environment")
-	}
+	// dbUrl, exists := os.LookupEnv("DATABASE_URL")
+	// if !exists {
+	// 	log.Fatal("DATABASE_URL not set in environment")
+	// }
 
 	internalServerPort, exists := os.LookupEnv("INTERNAL_SERVER_PORT")
 	if !exists {
@@ -43,6 +64,7 @@ func main() {
 		internalServerPort = "50051"
 	}
 
+	dbUrl := buildDatabaseURL()
 	db, err := connectToDB(dbUrl)
 	if err != nil {
 		log.Fatal(err)
